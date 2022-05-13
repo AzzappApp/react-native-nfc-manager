86737be5c75bc8b603b62f1bc3c7c91c0e0c7456
François de Campredon <francois.de.campredon@gmail.com>
2022-05-13 17:07:20 +0200

    Build IOS application with Github actions
    add setup_ci in fastlane

---
+++ packages/mobile/scripts/fix-m1-flipper-foly.sh
@@ -0,0 +1,3 @@
+rm ios/Pods/Flipper-Folly/folly/SharedMutex.cpp ios/Pods/Flipper-Folly/folly/SharedMutex.h
+cp ios/Pods/RCT-Folly/folly/SharedMutex.cpp ios/Pods/Flipper-Folly/folly/
+cp ios/Pods/RCT-Folly/folly/SharedMutex.h ios/Pods/Flipper-Folly/folly/
