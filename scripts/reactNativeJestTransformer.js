5b5296dd38b8cbd446d50f8747391f9efc7db7bb
François de Campredon <francois.de.campredon@gmail.com>
2022-06-15 14:30:24 +0200

    fix tests

--- scripts/reactNativeJestTransformer.js
+++ scripts/reactNativeJestTransformer.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const { createTransformer } = require('babel-jest');
 
+const relayArtifactDirectory = path.join(
+  path.dirname(require.resolve('@azzapp/relay/package.json')),
+  'artifacts',
+);
 module.exports = createTransformer({
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-    ['relay', { eagerEsModules: false }],
+    [
+      'relay',
+      { eagerEsModules: false, artifactDirectory: relayArtifactDirectory },
+    ],
   ],
 });
