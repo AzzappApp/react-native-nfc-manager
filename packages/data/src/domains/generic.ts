e78d851c42ba9aa1c6c4b35f20392f7248594011
Mickael Lecoq <mickael@lecoq.me>
2023-06-12 16:14:32 +0200

    fix: set correct database type for tinyint (#489)

--- packages/data/src/domains/generic.ts
+++ packages/data/src/domains/generic.ts
@@ -11,5 +11,5 @@ export const customTinyInt = (name: string) =>
   customType<{ data: boolean; driverData: 0 | 1 }>({
     toDriver: value => (value ? 1 : 0),
     fromDriver: value => Boolean(value),
-    dataType: () => 'tinyint',
+    dataType: () => 'tinyint(1)',
   })(name);
