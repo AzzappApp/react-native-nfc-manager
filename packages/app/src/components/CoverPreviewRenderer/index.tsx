0fcb5cbf93a137f609517105e0e6988a6a9a0c93
François de Campredon <francois.de.campredon@gmail.com>
2023-09-02 16:35:50 +0200

    fix typ errors

--- packages/app/src/components/CoverPreviewRenderer/index.tsx
+++ packages/app/src/components/CoverPreviewRenderer/index.tsx
@@ -1,8 +1,6 @@
 import CoverMediaPreview from './CoverMediaPreview';
 import CoverPreviewRenderer from './CoverPreviewRenderer';
-import type { CoverPreviewHandler } from './CoverPreviewRenderer';
 
 export default CoverPreviewRenderer;
 
 export { CoverMediaPreview };
-export type { CoverPreviewHandler };
