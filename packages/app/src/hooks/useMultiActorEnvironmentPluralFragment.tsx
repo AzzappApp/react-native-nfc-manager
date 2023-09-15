2f3a497c80aeee77691ffdd538f70d88c6bb106e
François de Campredon <francois.de.campredon@gmail.com>
2023-09-15 12:58:57 +0200

    fix(Home): informations not updated (#1126)

--- packages/app/src/hooks/useMultiActorEnvironmentPluralFragment.tsx
+++ packages/app/src/hooks/useMultiActorEnvironmentPluralFragment.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { ReactRelayContext } from 'react-relay';
 import { getFragment, getSelector, type FragmentType } from 'relay-runtime';
 import type { Environment, GraphQLTaggedNode } from 'react-relay';
@@ -7,7 +7,7 @@ const useMultiActorEnvironmentPluralFragment = <TKey extends KeyType>(
   fragmentInput: GraphQLTaggedNode,
   getActorID: (value: TKey) => string,
   fragmentRefs: readonly TKey[] | null | undefined,
-): ReadonlyArray<TKey[' $data']> | null => {
+): ReadonlyArray<Exclude<TKey[' $data'], undefined>> | null => {
   const [, forceUpdate] = useState(0);
   const fragment = useMemo(() => getFragment(fragmentInput), [fragmentInput]);
   const context = useContext(ReactRelayContext) as any;
@@ -21,30 +21,41 @@ const useMultiActorEnvironmentPluralFragment = <TKey extends KeyType>(
     );
   }
 
-  const snapshotWithEnvironments = useMemo(
+  const getActorIDRef = useRef(getActorID);
+  getActorIDRef.current = getActorID;
+
+  const fragmentsInfos = useMemo(
     () =>
       fragmentRefs?.map(fragmentRef => {
         const selector = getSelector(fragment, fragmentRef);
-        const environment = getEnvironmentForActor(getActorID(fragmentRef));
+        const environment = getEnvironmentForActor(
+          getActorIDRef.current(fragmentRef),
+        );
         const snapshot = environment.lookup(selector as any);
-        return { snapshot, environment };
+        return { snapshot, environment, selector };
       }),
-    [fragment, fragmentRefs, getActorID, getEnvironmentForActor],
+    [fragment, fragmentRefs, getEnvironmentForActor],
   );
 
   useEffect(() => {
-    const subscriptions = snapshotWithEnvironments?.map(
-      ({ snapshot, environment }) =>
-        environment.subscribe(snapshot, () => forceUpdate(i => i + 1)),
+    const subscriptions = fragmentsInfos?.map(({ snapshot, environment }) =>
+      environment.subscribe(snapshot, () => {
+        forceUpdate(i => i + 1);
+      }),
     );
     return () => {
       subscriptions?.forEach(subscription => {
         subscription.dispose();
       });
     };
-  }, [snapshotWithEnvironments]);
+  }, [fragmentsInfos]);
 
-  return snapshotWithEnvironments?.map(({ snapshot }) => snapshot.data) ?? null;
+  return (
+    fragmentsInfos?.map(
+      ({ environment, selector }) =>
+        environment.lookup(selector as any).data as any,
+    ) ?? null
+  );
 };
 
 type KeyType<TData = unknown> = Readonly<{
