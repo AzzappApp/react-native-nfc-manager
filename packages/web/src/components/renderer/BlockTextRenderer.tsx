92aa9a2fddac6218a55e728ac0ffed1b96e147be
Mickael Lecoq <mickael@lecoq.me>
2024-01-05 10:13:48 +0100

    fix(webCard): #2028 background resize mode was always set to cover (#2161)

--- packages/web/src/components/renderer/BlockTextRenderer.tsx
+++ packages/web/src/components/renderer/BlockTextRenderer.tsx
@@ -58,6 +58,7 @@ const BlockTextRenderer = ({
     >
       <div style={{ maxWidth: '800px', margin: 'auto' }}>
         <CardModuleBackground
+          {...props}
           backgroundId={textBackgroundId}
           backgroundStyle={textBackgroundStyle}
           colorPalette={colorPalette}
