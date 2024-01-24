8274af226c3653df2d9decaddc049e24dd9018f1
Mickael Lecoq <mickael@lecoq.me>
2024-01-24 10:29:32 +0100

    perf: don’t render other tabs on android (#2468)

--- packages/app/src/ui/TabView.tsx
+++ packages/app/src/ui/TabView.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 import type { ReactElement } from 'react';
 import type { ViewProps } from 'react-native';
 
@@ -9,21 +9,23 @@ export type TabViewProps = Omit<ViewProps, 'children'> & {
 
 const TabView = ({ tabs, currentTab, style, ...props }: TabViewProps) => (
   <View {...props} style={[{ overflow: 'hidden' }, style]}>
-    {tabs.map(({ id, element }) => (
-      <View
-        key={id}
-        style={[
-          StyleSheet.absoluteFill,
-          id !== currentTab && { opacity: 0, zIndex: -1 },
-        ]}
-        accessibilityRole="tab"
-        accessibilityState={{ selected: id === currentTab }}
-        aria-hidden={id !== currentTab}
-        pointerEvents={id === currentTab ? 'auto' : 'none'}
-      >
-        {element}
-      </View>
-    ))}
+    {tabs.map(({ id, element }) =>
+      Platform.OS === 'android' && id !== currentTab ? null : (
+        <View
+          key={id}
+          style={[
+            StyleSheet.absoluteFill,
+            id !== currentTab && { opacity: 0, zIndex: -1 },
+          ]}
+          accessibilityRole="tab"
+          accessibilityState={{ selected: id === currentTab }}
+          aria-hidden={id !== currentTab}
+          pointerEvents={id === currentTab ? 'auto' : 'none'}
+        >
+          {element}
+        </View>
+      ),
+    )}
   </View>
 );
 
