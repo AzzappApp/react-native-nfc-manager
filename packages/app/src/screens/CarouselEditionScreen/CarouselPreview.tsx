75282473320253943c21f0c565fb061c070f1622
Teddy MORIN <morin.td@gmail.com>
2024-01-25 16:15:55 +0100

    fix: 🐛 correctly display carousel preview (#2487)
    * fix: 🐛 correctly display carousel preview
    
    * fix: 🐛 correctly resize preview
    
    ---------
    
    Co-authored-by: Mickael Lecoq <mickael@lecoq.me>

--- packages/app/src/screens/CarouselEditionScreen/CarouselPreview.tsx
+++ packages/app/src/screens/CarouselEditionScreen/CarouselPreview.tsx
@@ -1,6 +1,6 @@
-import { View } from 'react-native';
-import { colors, shadow } from '#theme';
+import { shadow } from '#theme';
 import CarouselRenderer from '#components/cardModules/CarouselRenderer';
+import EditorScaledPreview from '#components/EditorScaledPreview';
 import { createStyleSheet, useStyleSheet } from '#helpers/createStyles';
 import type { CarouselRendererData } from '#components/cardModules/CarouselRenderer';
 import type { CardStyle, ColorPalette } from '@azzapp/shared/cardHelpers';
@@ -35,45 +35,22 @@ const CarouselPreview = ({
   height,
   ...props
 }: CarouselPreviewProps) => {
-  const { imageHeight, marginVertical, borderWidth } = data;
-  const moduleHeight =
-    (imageHeight ?? 200) + (marginVertical ?? 20) * 2 + (borderWidth ?? 0) * 2;
-  const scale = Math.min(height / moduleHeight, 1);
-
   const styles = useStyleSheet(styleSheet);
   return (
-    <View {...props}>
-      <View
-        style={[
-          {
-            position: 'absolute',
-            backgroundColor: colors.white,
-            minHeight: height,
-            top: scale === 1 ? 0 : (height - moduleHeight) / 2,
-            left: `${(100 - 100 / scale) / 2}%`,
-            width: `${100 / scale}%`,
-            transform: [{ scale }],
-            overflow: 'visible',
-            alignItems: 'center',
-            justifyContent: 'center',
-          },
-          styles.container,
-        ]}
-      >
-        <CarouselRenderer
-          data={data}
-          colorPalette={colorPalette}
-          cardStyle={cardStyle}
-          style={styles.module}
-        />
-      </View>
-    </View>
+    <EditorScaledPreview {...props}>
+      <CarouselRenderer
+        data={data}
+        colorPalette={colorPalette}
+        cardStyle={cardStyle}
+        style={styles.module}
+      />
+    </EditorScaledPreview>
   );
 };
 
 export default CarouselPreview;
 
 const styleSheet = createStyleSheet(apperance => ({
-  module: [{ width: '100%' }, shadow(apperance)],
+  module: [{ width: '100%' }],
   container: shadow(apperance),
 }));
