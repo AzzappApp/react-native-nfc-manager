44341b8fec94d151fba2da9764198ae61a799e40
François de Campredon <francois.de.campredon@gmail.com>
2024-02-02 11:49:48 +0100

    fix(ShakeAndShare): sensibility and error (#2616)
    * fix(ShakeAndShare): sensibility and error
    
    * disabled shake and share when profile info doesn't allows it
    
    ---------
    
    Co-authored-by: Mickael Lecoq <mickael@lecoq.me>

--- packages/app/src/components/ShakeShare.tsx
+++ packages/app/src/components/ShakeShare.tsx
@@ -1,5 +1,5 @@
 import { BlurView } from 'expo-blur';
-import { useCallback, useMemo, useState } from 'react';
+import { Suspense, useCallback, useMemo, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { View, StyleSheet, useWindowDimensions } from 'react-native';
 import {
@@ -10,16 +10,20 @@ import {
 } from 'react-native-gesture-handler';
 import QRCode from 'react-native-qrcode-svg';
 import {
-  useAnimatedSensor,
   SensorType,
-  useDerivedValue,
   runOnJS,
+  useAnimatedReaction,
+  useAnimatedSensor,
+  useDerivedValue,
+  useSharedValue,
 } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { graphql, useClientQuery } from 'react-relay';
 import { buildUserUrlWithContactCard } from '@azzapp/shared/urlHelpers';
 import { colors } from '#theme';
 import useAuthState from '#hooks/useAuthState';
+import useLatestCallback from '#hooks/useLatestCallback';
+import ActivityIndicator from '#ui/ActivityIndicator';
 import Header from '#ui/Header';
 import IconButton from '#ui/IconButton';
 import Text from '#ui/Text';
@@ -27,6 +31,53 @@ import CoverRenderer from './CoverRenderer';
 import type { ShakeShareScreenQuery } from '#relayArtifacts/ShakeShareScreenQuery.graphql';
 
 const ShakeShare = () => {
+  const [mountScreen, setMountScreen] = useState(false);
+  const { profileInfos } = useAuthState();
+
+  const dismount = useCallback(() => {
+    setMountScreen(false);
+  }, []);
+
+  useShakeDetector(() => {
+    if (profileInfos && profileInfos.profileRole !== 'invited') {
+      setMountScreen(true);
+    }
+  });
+
+  //Gesture to close on swipe
+
+  const fling = Gesture.Fling()
+    .direction(Directions.DOWN)
+    .runOnJS(true)
+    .onEnd(dismount);
+
+  if (!mountScreen) {
+    return null;
+  }
+  return (
+    <GestureHandlerRootView style={StyleSheet.absoluteFillObject}>
+      <GestureDetector gesture={fling}>
+        <BlurView intensity={27} tint="dark" style={styles.blurView}>
+          <SafeAreaView style={styles.safeArea}>
+            <Suspense
+              fallback={
+                <View style={styles.activityIndicatorContainer}>
+                  <ActivityIndicator color="white" />
+                </View>
+              }
+            >
+              <ShakeShareDisplay onClose={dismount} />
+            </Suspense>
+          </SafeAreaView>
+        </BlurView>
+      </GestureDetector>
+    </GestureHandlerRootView>
+  );
+};
+
+export default ShakeShare;
+
+const ShakeShareDisplay = ({ onClose }: { onClose: () => void }) => {
   const { profileInfos } = useAuthState();
   const { node } = useClientQuery<ShakeShareScreenQuery>(
     graphql`
@@ -52,17 +103,6 @@ const ShakeShare = () => {
 
   const profile = node?.profile;
 
-  const [mountScreen, setMountScreen] = useState(false);
-  const gyroscope = useAnimatedSensor(SensorType.ACCELEROMETER);
-
-  useDerivedValue(() => {
-    const { y, z } = gyroscope.sensor.value;
-    const speed = Math.sqrt(y * y + z * z);
-    if (speed > 30) {
-      runOnJS(setMountScreen)(true);
-    }
-  });
-
   const contactCardUrl = useMemo(() => {
     if (!profile?.serializedContactCard) {
       return null;
@@ -76,71 +116,47 @@ const ShakeShare = () => {
   }, [profile?.serializedContactCard, profile?.webCard]);
 
   const { width } = useWindowDimensions();
-
-  const dismount = useCallback(() => {
-    setMountScreen(false);
-  }, []);
-
-  //Gesture to close on swipe
-
-  const fling = Gesture.Fling()
-    .direction(Directions.DOWN)
-    .onEnd(() => {
-      runOnJS(dismount)();
-    });
-
-  if (!mountScreen) {
-    return null;
-  }
   return (
-    <GestureHandlerRootView style={StyleSheet.absoluteFillObject}>
-      <GestureDetector gesture={fling}>
-        <BlurView intensity={27} tint="dark" style={styles.blurView}>
-          <SafeAreaView style={styles.safeArea}>
-            <Header
-              leftElement={
-                <IconButton
-                  icon="close"
-                  variant="icon"
-                  onPress={dismount}
-                  iconStyle={styles.iconStyle}
-                />
-              }
-              middleElement={
-                <Text variant="large" style={styles.headerTitle}>
-                  <FormattedMessage
-                    defaultMessage={'Share your information'}
-                    description={'Shake share screen header title'}
-                  />
-                </Text>
-              }
-              style={styles.headerStyle}
+    <>
+      <Header
+        leftElement={
+          <IconButton
+            icon="close"
+            variant="icon"
+            onPress={onClose}
+            iconStyle={styles.iconStyle}
+          />
+        }
+        middleElement={
+          <Text variant="large" style={styles.headerTitle}>
+            <FormattedMessage
+              defaultMessage={'Share your information'}
+              description={'Shake share screen header title'}
             />
-            <View style={styles.container}>
-              <CoverRenderer
-                width={width / 2.5}
-                webCard={profile?.webCard}
-                style={styles.coverStyle}
-                animationEnabled={true}
-              />
-              {contactCardUrl && (
-                <QRCode
-                  value={contactCardUrl}
-                  size={width * 0.6}
-                  color={colors.white}
-                  backgroundColor="transparent"
-                  ecl="L"
-                />
-              )}
-            </View>
-          </SafeAreaView>
-        </BlurView>
-      </GestureDetector>
-    </GestureHandlerRootView>
+          </Text>
+        }
+        style={styles.headerStyle}
+      />
+      <View style={styles.container}>
+        <CoverRenderer
+          width={width / 2.5}
+          webCard={profile?.webCard}
+          style={styles.coverStyle}
+          animationEnabled={true}
+        />
+        {contactCardUrl && (
+          <QRCode
+            value={contactCardUrl}
+            size={width * 0.6}
+            color={colors.white}
+            backgroundColor="transparent"
+            ecl="L"
+          />
+        )}
+      </View>
+    </>
   );
 };
-
-export default ShakeShare;
 
 const styles = StyleSheet.create({
   blurView: { flex: 1 },
@@ -148,9 +164,90 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgba(14, 18, 22, 0.95)',
   },
+  activityIndicatorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   headerStyle: { backgroundColor: 'transparent' },
   iconStyle: { tintColor: colors.white },
   coverStyle: { marginBottom: 50 },
   headerTitle: { color: colors.white },
   container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
 });
+
+// This code is transposed from https://github.com/facebook/react-native/blob/184b295a019e2af6712d34276c741e0dae78f798/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/ShakeDetector.java#L45
+const GRAVITY_EARTH = 9.80665;
+const REQUIRED_FORCE = GRAVITY_EARTH * 1.8;
+
+const atLeastRequiredForce = (a: number) => {
+  'worklet';
+  return Math.abs(a) > REQUIRED_FORCE;
+};
+
+const useShakeDetector = (callback: () => void) => {
+  const lastShakeTimestamp = useSharedValue(0);
+  const numShakes = useSharedValue(0);
+  const accelX = useSharedValue(0);
+  const accelY = useSharedValue(0);
+  const accelZ = useSharedValue(0);
+
+  const reset = () => {
+    'worklet';
+    lastShakeTimestamp.value = 0;
+    numShakes.value = 0;
+    accelX.value = 0;
+    accelY.value = 0;
+    accelZ.value = 0;
+  };
+
+  const gyroscope = useAnimatedSensor(SensorType.ACCELEROMETER);
+
+  const recordShake = (timestamp: number) => {
+    'worklet';
+    lastShakeTimestamp.value = timestamp;
+    numShakes.value++;
+  };
+
+  const callbackRef = useLatestCallback(callback);
+  const maybeDispatchShake = (timeStamp: number) => {
+    'worklet';
+    if (numShakes.value >= 8) {
+      reset();
+      runOnJS(callbackRef)();
+    }
+
+    if (timeStamp - lastShakeTimestamp.value > 3000) {
+      reset();
+    }
+  };
+
+  const sensorValue = useDerivedValue(() => {
+    const { x, y, z } = gyroscope.sensor.value;
+    return {
+      ax: x,
+      ay: y,
+      az: z,
+    };
+  });
+
+  useAnimatedReaction(
+    () => sensorValue.value,
+    ({ ax, ay, az }) => {
+      const timeStamp = Date.now();
+
+      if (atLeastRequiredForce(ax) && ax * accelX.value <= 0) {
+        recordShake(timeStamp);
+        accelX.value = ax;
+      } else if (atLeastRequiredForce(ay) && ay * accelY.value <= 0) {
+        recordShake(timeStamp);
+        accelX.value = ay;
+      } else if (atLeastRequiredForce(az) && az * accelZ.value <= 0) {
+        recordShake(timeStamp);
+        accelX.value = az;
+      }
+
+      maybeDispatchShake(timeStamp);
+    },
+  );
+};
