e346d6263a0f749de86d63051b3bf4c7c88e4da9
fahbe <shecart.mydev@gmail.com>
2024-02-20 18:25:08 +0100

    fix: (SortableList) fix order if moving for more than one position at a time (#2857)

--- packages/app/src/components/SortableScrollView/SortableListHelper.ts
+++ packages/app/src/components/SortableScrollView/SortableListHelper.ts
@@ -5,14 +5,21 @@ export function objectMove(
 ) {
   'worklet';
   const newObject = Object.assign({}, object);
+  // when we move an object from index 'from' to index 'to',
+  // we need to update the position of the other object between them and not only the one it si replacing
+
+  const direction = from < to ? 'up' : 'down';
+
   // eslint-disable-next-line guard-for-in
   for (const id in object) {
     if (object[id] === from) {
       newObject[id] = to;
     }
-
-    if (object[id] === to) {
-      newObject[id] = from;
+    if (direction === 'up' && object[id] > from && object[id] <= to) {
+      newObject[id] = object[id] - 1;
+    }
+    if (direction === 'down' && object[id] < from && object[id] >= to) {
+      newObject[id] = object[id] + 1;
     }
   }
   return newObject;
