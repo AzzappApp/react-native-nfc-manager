f1a7095b981ad5bf2cfa9ec1f3b1aaef3acf7833
Mickael Lecoq <mickael@lecoq.me>
2024-02-27 15:15:47 +0100

    fix(cover): #2863 return default cover when no one is registered (#2919)

--- packages/web/src/app/api/cover/[username]/route.tsx
+++ packages/web/src/app/api/cover/[username]/route.tsx
@@ -457,7 +457,50 @@ export const GET = async (
         },
       );
     } else {
-      return NextResponse.json({ message: ERRORS.NOT_FOUND }, { status: 404 });
+      return new ImageResponse(
+        (
+          <div
+            style={{
+              display: 'flex',
+              width,
+              height,
+              backgroundColor: 'transparent',
+              alignItems: crop === 'lpad' ? 'flex-end' : 'center',
+              flexDirection: 'column',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flex: 1,
+                position: 'relative',
+                maxWidth: imageWidth,
+              }}
+            >
+              <svg
+                height="100%"
+                width="100%"
+                viewBox="0 0 221 332"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g>
+                  <rect width="100%" height="100%" fill="black" />
+                  <path
+                    opacity="0.2"
+                    d="M110.5 188C113.52 188 116.442 187.582 119.212 186.8C121.132 186.258 123.245 186.959 124.242 188.686L126.534 192.657C121.626 194.807 116.202 196 110.5 196C88.4086 196 70.5 178.091 70.5 156C70.5 133.909 88.4086 116 110.5 116C132.591 116 150.5 133.909 150.5 156C150.5 169.529 143.784 181.489 133.504 188.728L110.764 149.357L102.31 164C100.881 166.475 98.2398 168 95.3817 168H90.7629L110.764 133.357L135.425 176.071C139.851 170.581 142.5 163.6 142.5 156C142.5 138.327 128.173 124 110.5 124C92.8269 124 78.5 138.327 78.5 156C78.5 173.673 92.8269 188 110.5 188Z"
+                    fill="white"
+                  />
+                </g>
+              </svg>
+            </div>
+          </div>
+        ),
+        {
+          width,
+          height,
+        },
+      );
     }
   } else {
     return NextResponse.json({ message: ERRORS.FORBIDDEN }, { status: 403 });
