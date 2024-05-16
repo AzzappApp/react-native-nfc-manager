eaf4217f22204f380080e3ab3fb4e0f633e5b444
Mickael Lecoq <mickael@lecoq.me>
2024-05-16 12:22:31 +0200

    fix(backoffice): set lifetime subscription

--- packages/backoffice/src/app/(main)/users/[id]/subscriptionActions.ts
+++ packages/backoffice/src/app/(main)/users/[id]/subscriptionActions.ts
@@ -1,23 +1,55 @@
 'use server';
 
+import { and, eq } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
-import { upsertSubscription } from '@azzapp/data';
+import { UserSubscriptionTable, createSubscription, db } from '@azzapp/data';
+
 import { createId } from '@azzapp/data/helpers/createId';
 
 export const setLifetimeSubscription = async (
   userId: string,
   active: boolean,
 ) => {
-  await upsertSubscription({
-    userId,
-    subscriptionPlan: 'web.lifetime',
-    subscriptionId: createId(),
-    startAt: new Date(),
-    endAt: new Date('2099-12-31'),
-    issuer: 'web',
-    totalSeats: 999999,
-    status: active ? 'active' : 'canceled',
-    canceledAt: active ? null : new Date(),
+  await db.transaction(async trx => {
+    const existingLifetimeSubscription = await db
+      .select()
+      .from(UserSubscriptionTable)
+      .where(
+        and(
+          eq(UserSubscriptionTable.userId, userId),
+          eq(UserSubscriptionTable.subscriptionPlan, 'web.lifetime'),
+        ),
+      );
+
+    if (existingLifetimeSubscription.length > 0) {
+      await trx
+        .update(UserSubscriptionTable)
+        .set({
+          status: active ? 'active' : 'canceled',
+          canceledAt: active ? null : new Date(),
+        })
+        .where(
+          and(
+            eq(UserSubscriptionTable.userId, userId),
+            eq(UserSubscriptionTable.subscriptionPlan, 'web.lifetime'),
+          ),
+        );
+    } else {
+      await createSubscription(
+        {
+          userId,
+          subscriptionPlan: 'web.lifetime',
+          subscriptionId: createId(),
+          startAt: new Date(),
+          endAt: new Date('2099-12-31'),
+          issuer: 'web',
+          totalSeats: 999999,
+          status: active ? 'active' : 'canceled',
+          canceledAt: active ? null : new Date(),
+        },
+        trx,
+      );
+    }
   });
   revalidatePath(`/users/${userId}`);
 };
