2de7c40ea6dfece00974f23885ef9fdda9185e94
Mickael Lecoq <mickael@lecoq.me>
2024-05-24 16:33:44 +0200

    build: move payment script to payment package (#3390)

--- packages/payment/scripts/generatePaymentApiClient.js
+++ packages/payment/scripts/generatePaymentApiClient.js
@@ -4,13 +4,10 @@ const openapiTS = require('openapi-typescript');
 console.log('Generating payment API client...');
 console.log('API URL:', process.env.PAYMENT_API_URL);
 
-fs.mkdirSync('./packages/payment/src/__generated__', { recursive: true });
+fs.mkdirSync('./src/__generated__', { recursive: true });
 
 openapiTS(`${process.env.PAYMENT_API_URL}/api/docs.jsonopenapi`).then(
   output => {
-    fs.writeFileSync(
-      './packages/payment/src/__generated__/paymentApi.ts',
-      output,
-    );
+    fs.writeFileSync('./src/__generated__/paymentApi.ts', output);
   },
 );
