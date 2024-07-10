bc693a32add6e291becd83f9d2557378b83b3dc4
Mickael Lecoq <mickael@lecoq.me>
2024-07-10 15:44:21 +0200

    fix(mediaPicker): #4152 display loader on icloud files (#4157)

--- packages/app/src/components/PhotoGalleryMediaList.tsx
+++ packages/app/src/components/PhotoGalleryMediaList.tsx
@@ -270,8 +270,8 @@ const PhotoGalleryMediaList = ({
   }, [load]);
 
   const extraData = useMemo(
-    () => ({ selectedMediasIds, selectedMediaId }),
-    [selectedMediasIds, selectedMediaId],
+    () => ({ selectedMediasIds, selectedMediaId, downloadingFiles }),
+    [selectedMediasIds, selectedMediaId, downloadingFiles],
   );
 
   const [manageAccessMediaVisible, toggleManageAccessMediaVisible] =
