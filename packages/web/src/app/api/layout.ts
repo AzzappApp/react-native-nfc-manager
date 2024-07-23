5364f602cd88b0b7ff5c449fc067261789124609
Mickael Lecoq <mickael@lecoq.me>
2024-07-23 10:30:57 +0200

    chore: configure region properly

--- packages/web/src/app/api/layout.ts
+++ packages/web/src/app/api/layout.ts
@@ -1,8 +1,5 @@
 export const runtime = 'edge';
 
-export const preferredRegion =
-  process.env.NEXT_PUBLIC_PLATFORM === 'production'
-    ? ['fra1', 'iad1', 'pdx1', 'gru1', 'sin1']
-    : ['fra1'];
+export const preferredRegion = ['fra1', 'iad1', 'pdx1', 'gru1', 'sin1'];
 
 export const dynamic = 'force-dynamic';
