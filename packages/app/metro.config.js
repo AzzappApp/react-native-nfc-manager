7e28f6ab631dd4ab64ab32893134d8bbac5c74f2
Mickael Lecoq <mickael@lecoq.me>
2024-08-02 16:48:53 +0200

    fix: try alternate configuration for metro config

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -5,35 +5,33 @@
  * @format
  */
 const path = require('path');
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const {
-  createSentryMetroSerializer,
-} = require('@sentry/react-native/dist/js/tools/sentryMetroSerializer');
+const { mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const { withSentryConfig } = require('@sentry/react-native/metro');
 const {
   getMetroAndroidAssetsResolutionFix,
 } = require('react-native-monorepo-tools');
 
 const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), {
-  transformer: {
-    publicPath: androidAssetsResolutionFix.publicPath,
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  server: {
-    // ...and to the server middleware.
-    enhanceMiddleware: middleware => {
-      return androidAssetsResolutionFix.applyMiddleware(middleware);
+module.exports = withSentryConfig(
+  mergeConfig(getDefaultConfig(__dirname), {
+    transformer: {
+      publicPath: androidAssetsResolutionFix.publicPath,
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
     },
-  },
-  serializer: {
-    customSerializer: createSentryMetroSerializer(),
-  },
-  watchFolders: [path.resolve(__dirname, '../../')],
-});
+    server: {
+      // ...and to the server middleware.
+      enhanceMiddleware: middleware => {
+        return androidAssetsResolutionFix.applyMiddleware(middleware);
+      },
+    },
+    watchFolders: [path.resolve(__dirname, '../../')],
+  }),
+);
