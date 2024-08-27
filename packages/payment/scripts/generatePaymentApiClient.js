18be1c0b806e5b4173a5a19c864e925f11de6fbc
Mickael Lecoq <mickael@lecoq.me>
2024-08-27 18:12:08 +0200

    Revert "chore: disable payment generation for the moment" (#4769)
    * Revert "chore: disable payment generation for the moment"
    
    This reverts commit 738886c4e58e59f56ff1b5874f87f52662515acc.
    
    * chore: add logs
    
    * remove useless dependency
    
    * don’t rerun api client build

--- packages/payment/scripts/generatePaymentApiClient.js
+++ packages/payment/scripts/generatePaymentApiClient.js
@@ -6,8 +6,8 @@ console.log('API URL:', process.env.PAYMENT_API_URL);
 
 fs.mkdirSync('./src/__generated__', { recursive: true });
 
-openapiTS(`${process.env.PAYMENT_API_URL}/api/docs.jsonopenapi`).then(
-  output => {
+openapiTS(`${process.env.PAYMENT_API_URL}/api/docs.jsonopenapi`)
+  .then(output => {
     fs.writeFileSync('./src/__generated__/paymentApi.ts', output);
-  },
-);
+  })
+  .catch(console.error);
