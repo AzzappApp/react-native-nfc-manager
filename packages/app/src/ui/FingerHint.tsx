b0e4de455e8e78ae7ca06b32e94dcda517f399cd
fahbe <shecart.mydev@gmail.com>
2024-09-13 12:10:27 +0200

    fix: click through finger hint (#5004)
    Co-authored-by: Mickael Lecoq <mickael@lecoq.me>

--- packages/app/src/ui/FingerHint.tsx
+++ packages/app/src/ui/FingerHint.tsx
@@ -1,6 +1,6 @@
 import LottieView from 'lottie-react-native';
 import { memo } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import type { ViewStyle } from 'react-native';
 type FingerHintProps = {
   color: 'dark' | 'light';
@@ -8,26 +8,32 @@ type FingerHintProps = {
 };
 const FingerHint = ({ color, style }: FingerHintProps) => {
   return (
-    <LottieView
-      source={
-        color === 'dark'
-          ? require('#assets/hint_hand_black.json')
-          : require('#assets/hint_hand_white.json')
-      }
-      autoPlay
-      loop
-      style={[styles.lottie, style]}
-    />
+    <View style={styles.container}>
+      <LottieView
+        source={
+          color === 'dark'
+            ? require('#assets/hint_hand_black.json')
+            : require('#assets/hint_hand_white.json')
+        }
+        autoPlay
+        loop
+        style={[styles.lottie, style]}
+      />
+    </View>
   );
 };
 
 export default memo(FingerHint);
 
 const styles = StyleSheet.create({
-  lottie: {
+  container: {
     width: 150,
     height: 187.5,
     position: 'absolute',
     pointerEvents: 'none',
+  },
+  lottie: {
+    width: 150,
+    height: 187.5,
   },
 });
