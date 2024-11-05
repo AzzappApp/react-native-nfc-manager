6650efdb9dc50ccd46f24675a4dff158f7a898ae
Mickael Lecoq <mickael@lecoq.me>
2024-11-05 09:34:22 +0100

    fix(home): #5649 radial gradient is hidden (#5656)
    * fix(home): #5649 radial gradient is hidden
    
    * put back Dimensions

--- packages/app/src/components/WebCardBackground.tsx
+++ packages/app/src/components/WebCardBackground.tsx
@@ -20,17 +20,17 @@ const WebCardBackground = ({
     <View style={StyleSheet.absoluteFill}>
       <Canvas style={{ flex: 1 }}>
         <Rect x={0} y={0} width={width} height={height}>
-          <RadialGradient
-            c={vec(width / 2, 0)}
-            r={width * 1.3}
-            colors={colors}
-          />
-        </Rect>
-        <Rect x={0} y={0} width={width} height={height}>
           <LinearGradient
             start={vec(width / 2, 0)}
             end={vec(width / 2, height * 0.66)}
             positions={[0.22, 0.66]}
+            colors={colors}
+          />
+        </Rect>
+        <Rect x={0} y={0} width={width} height={height}>
+          <RadialGradient
+            c={vec(width / 2, 0)}
+            r={width * 1.3}
             colors={colors}
           />
         </Rect>
