988aba15313151dd45915fa042c5c447f5f658b9
Teddy MORIN <morin.td@gmail.com>
2024-11-15 12:27:50 +0100

    fix: 🐛 avoid searchbar overflow (#5939)
    Co-authored-by: Mickael Lecoq <mickael@lecoq.me>

--- packages/app/src/ui/SearchBar.tsx
+++ packages/app/src/ui/SearchBar.tsx
@@ -227,6 +227,7 @@ const styleSheet = createStyleSheet(appearance => ({
     borderRadius: 12,
     fontSize: 16,
     color: appearance === 'light' ? colors.black : colors.grey400,
+    flex: 1,
   },
   input: {
     flex: 1,
@@ -241,6 +242,7 @@ const styleSheet = createStyleSheet(appearance => ({
     justifyContent: 'flex-start',
     overflow: 'hidden',
     columnGap: 10,
+    paddingHorizontal: 5,
   },
   container: {
     overflow: 'hidden',
