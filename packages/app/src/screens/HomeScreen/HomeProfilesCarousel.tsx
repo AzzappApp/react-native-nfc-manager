9370219aab51530a8212f75099107b08b8731d9f
Mickael Lecoq <mickael@lecoq.me>
2024-11-27 17:31:23 +0100

    Revert "Workaround/ios scroll issue afternew webcard created (#6093)"
    This reverts commit d1d520e52da8c46d92b0719aa3f628ab01d162a9.

--- packages/app/src/screens/HomeScreen/HomeProfilesCarousel.tsx
+++ packages/app/src/screens/HomeScreen/HomeProfilesCarousel.tsx
@@ -121,7 +121,6 @@ const HomeProfilesCarousel = ({ user: userKey }: HomeProfilesCarouselProps) => {
   // globalScrollToIndex is available in Context to allow scrolling
   // when a new card is added via invitation
   globalScrollToIndex.current = scrollToIndex;
-  const onFocusScrollTimeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
   useOnFocus(() => {
     const { profileInfos } = getAuthState();
@@ -133,21 +132,9 @@ const HomeProfilesCarousel = ({ user: userKey }: HomeProfilesCarouselProps) => {
       authProfileIndex !== -1 &&
       authProfileIndex + 1 !== currentIndexProfileSharedValue.get()
     ) {
-      if (Platform.OS === 'ios') {
-        clearTimeout(onFocusScrollTimeoutRef.current);
-        // This timeout is a workaround for: https://github.com/AzzappApp/azzapp/issues/6071
-        onFocusScrollTimeoutRef.current = setTimeout(() => {
-          carouselRef.current?.scrollToIndex(authProfileIndex + 1, false);
-        }, 2000);
-      } else {
-        carouselRef.current?.scrollToIndex(authProfileIndex + 1, false);
-      }
+      carouselRef.current?.scrollToIndex(authProfileIndex + 1, false);
     }
   });
-
-  useEffect(() => {
-    return () => clearTimeout(onFocusScrollTimeoutRef.current);
-  }, []);
 
   const carouselRef = useRef<CarouselSelectListHandle | null>(null);
   const [selectedIndex, setSelectedIndex] = useState(initialProfileIndex);
