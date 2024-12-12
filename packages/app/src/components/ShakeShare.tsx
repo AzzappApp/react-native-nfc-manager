e90b830bf4602746e2b1776163b8f49c04a9cba8
fahbe <shecart.mydev@gmail.com>
2024-12-12 19:52:58 +0100

    fix: close button on shake'n share (#6344)

--- packages/app/src/components/ShakeShare.tsx
+++ packages/app/src/components/ShakeShare.tsx
@@ -308,7 +308,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 200,
     alignItems: 'center',
-    pointerEvents: 'none',
+    pointerEvents: 'box-none',
   },
   button: {
     borderWidth: 1,
