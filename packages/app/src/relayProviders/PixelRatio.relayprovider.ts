a08331fea420f1bcba5a2f8bc0178f1a53705db2
Mickael Lecoq <mickael@lecoq.me>
2025-01-06 15:34:07 +0100

    Fix video quality on android (#6688)
    * fix(android): enhance media quality on high quality devices
    
    * remove console log

--- packages/app/src/relayProviders/PixelRatio.relayprovider.ts
+++ packages/app/src/relayProviders/PixelRatio.relayprovider.ts
@@ -1,7 +1,10 @@
 import { PixelRatio, Platform } from 'react-native';
+import { MEMORY_SIZE } from '#helpers/device';
 
 export const get = () => {
-  return Platform.OS === 'android' && process.env.JEST_WORKER_ID === undefined
+  return Platform.OS === 'android' &&
+    process.env.JEST_WORKER_ID === undefined &&
+    MEMORY_SIZE < 8
     ? 1
     : PixelRatio.get();
 };
