337d58aada20e235de1532da22197c1388dd4a38
Olivier Bouillet <62574056+freeboub@users.noreply.github.com>
2025-01-06 13:34:46 +0100

    fix: ensure avatar is always downloaded and improve log / sentry (#6649)
    * fix: backport fix avif fix and ensure avatar is always downloaded and improve log / sentry
    
    * fix: remove contact url rewrite
    
    * chore: replace log by warn
    
    * fix: replace log by warn
    
    ---------
    
    Co-authored-by: Olivier Bouillet <olivier@azzapp.com>

--- packages/app/src/screens/WebCardScreen/AddContactModal.tsx
+++ packages/app/src/screens/WebCardScreen/AddContactModal.tsx
@@ -307,21 +307,26 @@ const AddContactModal = ({
             }
 
             if (Platform.OS === 'ios') {
-              const updatedContact = reworkContactForDeviceInsert({
-                ...scanned.contact,
-                urlAddresses:
-                  additionalContactData?.urls?.map(addr => {
-                    return { label: 'default', address: addr.address };
-                  }) || undefined,
-                socialProfiles:
-                  additionalContactData?.socials?.map(social => {
-                    return { ...social, address: social.url };
-                  }) || undefined,
-              });
+              try {
+                const updatedContact = reworkContactForDeviceInsert({
+                  ...scanned.contact,
+                  urlAddresses:
+                    additionalContactData?.urls?.map(addr => {
+                      return { label: 'default', address: addr.address };
+                    }) || undefined,
+                  socialProfiles:
+                    additionalContactData?.socials?.map(social => {
+                      return { ...social, address: social.url };
+                    }) || undefined,
+                });
 
-              await presentFormAsync(null, updatedContact, {
-                isNew: true,
-              });
+                await presentFormAsync(null, updatedContact, {
+                  isNew: true,
+                });
+              } catch (e) {
+                console.warn(e);
+                Sentry.captureException(e);
+              }
             } else {
               router.push({
                 route: 'CONTACT_DETAILS',
@@ -529,10 +534,12 @@ const buildContact = async (
   if (additionalContactData?.avatarUrl) {
     try {
       const avatar = new File(Paths.cache.uri + profileId);
-      if (!avatar.exists) {
-        avatar.create();
-        await File.downloadFileAsync(additionalContactData.avatarUrl, avatar);
+      if (avatar.exists) {
+        // be sure that avatar can be refreshed
+        avatar.delete();
       }
+      avatar.create();
+      await File.downloadFileAsync(additionalContactData.avatarUrl, avatar);
 
       image = {
         width: 720,
@@ -540,6 +547,7 @@ const buildContact = async (
         uri: avatar.uri,
       };
     } catch (e) {
+      console.warn('error downloading avatar', e);
       Sentry.captureException(e);
     }
   }
