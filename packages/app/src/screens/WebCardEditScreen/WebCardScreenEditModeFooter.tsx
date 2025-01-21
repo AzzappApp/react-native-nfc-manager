8e0ef6d08d7b9e2bf09203683b493caea44bd083
Olivier Bouillet <62574056+freeboub@users.noreply.github.com>
2025-01-21 16:28:47 +0100

    fix: handle correctly paddingBottom in WebCardScreen (#7034)
    Co-authored-by: Olivier Bouillet <olivier@azzapp.com>

--- packages/app/src/screens/WebCardEditScreen/WebCardScreenEditModeFooter.tsx
+++ packages/app/src/screens/WebCardEditScreen/WebCardScreenEditModeFooter.tsx
@@ -6,6 +6,7 @@ import { graphql, useFragment } from 'react-relay';
 import { replaceColors } from '@azzapp/shared/lottieHelpers';
 import { colors, shadow } from '#theme';
 import { createStyleSheet, useStyleSheet } from '#helpers/createStyles';
+import useScreenInsets from '#hooks/useScreenInsets';
 import Button from '#ui/Button';
 import Text from '#ui/Text';
 import placeholder from '../../assets/module/placeholder_sections.json';
@@ -93,8 +94,11 @@ const WebCardScreenEditModeFooter = ({
     );
   }, [webCard.cardColors, webCard.coverBackgroundColor]);
 
+  const { bottom } = useScreenInsets();
   return (
-    <View style={[styles.root, styles.creation]}>
+    <View
+      style={[styles.root, styles.creation, { paddingBottom: bottom + 30 }]}
+    >
       {webCard.cardModules.length === 0 && (
         <View style={styles.lottieContainer}>
           <View style={styles.lottie}>
@@ -138,7 +142,6 @@ export const WEBCARD_SCREEN_EDIT_MODE_FOOTER_HEIGHT = 110;
 
 const stylesheet = createStyleSheet(theme => ({
   root: {
-    paddingTop: 30,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
