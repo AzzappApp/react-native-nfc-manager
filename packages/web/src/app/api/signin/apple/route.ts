310860c039bc923a8b362a3453b153b589286482
François de Campredon <francois.de.campredon@gmail.com>
2025-03-27 15:51:47 +0100

    Update route.ts

--- packages/web/src/app/api/signin/apple/route.ts
+++ packages/web/src/app/api/signin/apple/route.ts
@@ -54,6 +54,7 @@ const appleSignin = async (req: Request) => {
     );
   }
   const appleId = payload.sub;
+  console.log(payload);
   if (!appleId) {
     console.error('Apple Sign In: Invalid token');
     return NextResponse.json(
