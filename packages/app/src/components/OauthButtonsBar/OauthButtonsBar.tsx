6c7db41f0d8f4491d9b2cc24a62274a8b018d61f
Mickael Lecoq <mickael@lecoq.me>
2025-03-28 15:24:59 +0100

    fix(authent): remove suffix in scheme

--- packages/app/src/components/OauthButtonsBar/OauthButtonsBar.tsx
+++ packages/app/src/components/OauthButtonsBar/OauthButtonsBar.tsx
@@ -17,7 +17,7 @@ import type { AuthResponse } from '@azzapp/shared/WebAPI';
 
 WebBrowser.maybeCompleteAuthSession();
 const redirectUri = AuthSession.makeRedirectUri({
-  scheme: process.env.APP_SCHEME ?? 'azzapp',
+  scheme: process.env.APP_SCHEME?.replace('://', '') ?? 'azzapp',
   path: 'login',
 });
 
