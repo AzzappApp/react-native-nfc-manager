fd2785feab2056ba60bdf0e5f057d2c38716bb33
Mickael Lecoq <mickael@lecoq.me>
2025-03-28 17:17:39 +0100

    build: add missing signin key

--- packages/web/src/app/api/inngest/route.ts
+++ packages/web/src/app/api/inngest/route.ts
@@ -11,6 +11,7 @@ import {
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   functions: [
     sendEmailSignature,
     sendEmailSignatureBatch,
