c563a6ada2fe6a497254a2b90d282d9172cd2419
Mickael Lecoq <mickael@lecoq.me>
2025-04-07 15:36:54 +0200

    fix(oauth): apple token generation

--- packages/web/src/app/api/signin/apple/callback/route.ts
+++ packages/web/src/app/api/signin/apple/callback/route.ts
@@ -1,31 +1,31 @@
-import { SignJWT } from 'jose';
+import { importPKCS8, SignJWT } from 'jose';
 import { oauthSigninCallback } from '../../oauthSigninUtils';
 
-async function generateAppleClientSecret() {
+const APPLE_PRIVATE_KEY = process.env.APPLE_PRIVATE_KEY!;
+const APPLE_TEAM_ID = process.env.APPLE_TEAM_ID!;
+const APPLE_CLIENT_ID = process.env.APPLE_CLIENT_ID!;
+const APPLE_KEY_ID = process.env.APPLE_KEY_ID!;
+
+export async function generateAppleClientSecret() {
   const now = Math.floor(Date.now() / 1000);
+
+  const privateKey = await importPKCS8(APPLE_PRIVATE_KEY, 'ES256');
+
   const jwt = await new SignJWT({})
-    .setProtectedHeader({ alg: 'ES256', kid: process.env.APPLE_KEY_ID! })
-    .setIssuer(process.env.APPLE_TEAM_ID!)
-    .setIssuedAt(now)
-    .setExpirationTime(now + 60 * 60 * 6) // 6 heures
+    .setProtectedHeader({ alg: 'ES256', kid: APPLE_KEY_ID })
+    .setIssuer(APPLE_TEAM_ID)
+    .setSubject(APPLE_CLIENT_ID)
     .setAudience('https://appleid.apple.com')
-    .setSubject(process.env.APPLE_CLIENT_ID!)
-    .sign(
-      await crypto.subtle.importKey(
-        'pkcs8',
-        new TextEncoder().encode(process.env.APPLE_PRIVATE_KEY!),
-        { name: 'ECDSA', namedCurve: 'P-256' },
-        false,
-        ['sign'],
-      ),
-    );
+    .setIssuedAt(now)
+    .setExpirationTime(now + 60 * 60 * 6) // 6h
+    .sign(privateKey);
 
   return jwt;
 }
 
 const appleSigninCallback = oauthSigninCallback({
   tokenURL: 'https://appleid.apple.com/auth/token',
-  clientId: process.env.APPLE_CLIENT_ID!,
+  clientId: APPLE_CLIENT_ID,
   clientSecret: generateAppleClientSecret,
   redirectURI: `${process.env.NEXT_PUBLIC_API_ENDPOINT!}/signin/apple/callback`,
   csrfSecret: new TextEncoder().encode(process.env.APPLE_TOKEN_SECRET),
