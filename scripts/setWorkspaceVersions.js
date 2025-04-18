41d1a19d7ac0cb6860476a5146eddcaee753fd7f
François de Campredon <francois.de.campredon@gmail.com>
2025-04-18 09:53:20 +0200

    chore: sync native version in build instead of commiting in source

--- scripts/setWorkspaceVersions.js
+++ scripts/setWorkspaceVersions.js
@@ -1,12 +1,5 @@
-const { execSync } = require('child_process');
 const fs = require('fs');
-const path = require('path');
-const stripIndents = require('common-tags/lib/stripIndents');
 const { glob } = require('glob');
-const beautify = require('js-beautify').html;
-const Xcode = require('pbxproj-dom/xcode').Xcode;
-const plist = require('plist');
-const RNVersion = require('react-native-version');
 
 module.exports = function setWorkspaceVersions(
   kind,
@@ -56,15 +49,6 @@ module.exports = function setWorkspaceVersions(
     fs.writeFileSync(file, JSON.stringify(pgk, null, 2) + '\n');
   });
 
-  let androidVersionCode = 0;
-  const paddedMinor = `${minor}`.padStart(2, '0');
-  const paddedPatch = `${patch}`.padStart(2, '0');
-  const paddedPreReleaseNumber = `${internal}`.padStart(
-    kind === 'rc' || kind === 'canary' ? 3 : 1,
-    '0',
-  );
-  androidVersionCode = `${major}${paddedMinor}${paddedPatch}${paddedPreReleaseNumber}`;
-
   const internalVersionFile = require.resolve('../internal-version.json');
   fs.writeFileSync(
     internalVersionFile,
@@ -78,64 +62,5 @@ module.exports = function setWorkspaceVersions(
     ),
   );
 
-  execSync(
-    `yarn react-native-version -A -r --generate-build --skip-tag packages/app --set-build ${androidVersionCode}`,
-  );
-
-  const xcode = Xcode.open(
-    './packages/app/ios/azzapp.xcodeproj/project.pbxproj',
-  );
-  RNVersion.getPlistFilenames(xcode).forEach(name => {
-    const file = path.join('./packages/app/ios', name);
-
-    const json = plist.parse(fs.readFileSync(file, 'utf8'));
-    const newContent = plist.build(
-      Object.assign({}, json, { CFBundleVersion: `${internal}` }),
-    );
-
-    fs.writeFileSync(
-      file,
-      stripIndents`
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">` +
-        '\n' +
-        beautify(
-          newContent.match(/<dict>[\s\S]*<\/dict>/)[0],
-          Object.assign({ end_with_newline: true, indent_with_tabs: true }),
-        ) +
-        stripIndents`
-      </plist>` +
-        '\n',
-    );
-  });
-  updatePbxProject();
-
   console.log(`Updated packages to version ${version} (${kind}.${internal})`);
-};
-
-const updatePbxProject = () => {
-  const plistPath = path.join('./packages/app/ios/azzapp', 'Info.plist');
-  const plistContent = fs.readFileSync(plistPath, 'utf8');
-  const plistData = plist.parse(plistContent);
-  const buildVersion = plistData.CFBundleVersion;
-  const shortVersion = plistData.CFBundleShortVersionString;
-  const projectPath = path.join(
-    './packages/app/ios/azzapp.xcodeproj',
-    'project.pbxproj',
-  );
-  const xcode = Xcode.open(projectPath);
-  xcode.document.projects.forEach(project => {
-    project.targets.forEach(target => {
-      if (target.buildConfigurationsList) {
-        target.buildConfigurationsList.buildConfigurations.forEach(config => {
-          const buildSettings = config.ast.value.get('buildSettings');
-          buildSettings.set('MARKETING_VERSION', shortVersion);
-          buildSettings.set('CURRENT_PROJECT_VERSION', buildVersion);
-        });
-      }
-    });
-  });
-
-  xcode.save();
 };
