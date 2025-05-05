eeec299aa21a712703a21da28767107e2969bc65
Mickael Lecoq <mickael@lecoq.me>
2025-05-05 16:20:56 +0200

    fix: remove android app banner

--- packages/web/src/components/ClientWrapper.tsx
+++ packages/web/src/components/ClientWrapper.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { DEFAULT_LOCALE } from '@azzapp/i18n';
 import { intlErrorHandler } from '#helpers/i18nHelpers';
-import AndroidAppDownloadBanner from './AndroidAppDownloadBanner';
 
 export type ClientWrapperProps = {
   children: IntlProvider['props']['children'];
@@ -20,7 +19,6 @@ const ClientWrapper = ({ children, locale, messages }: ClientWrapperProps) => {
       messages={messages}
       onError={intlErrorHandler}
     >
-      <AndroidAppDownloadBanner />
       {children}
     </IntlProvider>
   );
