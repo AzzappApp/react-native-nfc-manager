53a354dde33552e7232f135e102e0a93d7e3c2eb
Sebastien Hecart <shecart.mydev@gmail.com>
2025-05-07 10:54:40 +0200

    fix: publish android paywall (#8697)

--- packages/app/src/screens/HomeScreen/HomeBottomPanelPublish.tsx
+++ packages/app/src/screens/HomeScreen/HomeBottomPanelPublish.tsx
@@ -60,23 +60,7 @@ const HomeBottomPanelPublish = ({ profile }: HomeBottomPanelPublishProps) => {
 
     if (profile.webCard?.requiresSubscription && !profile.webCard.isPremium) {
       if (profileInfos?.profileRole === 'owner') {
-        if (Platform.OS === 'android') {
-          Toast.show({
-            type: 'error',
-            text1: intl.formatMessage(
-              {
-                defaultMessage: 'You can’t publish with WebCard{azzappA}.',
-                description:
-                  'Error toast message when user tries to publish a premium contact card on android without a subscription',
-              },
-              {
-                azzappA: <Text variant="azzapp">a</Text>,
-              },
-            ) as unknown as string,
-          });
-        } else {
-          router.push({ route: 'USER_PAY_WALL' });
-        }
+        router.push({ route: 'USER_PAY_WALL' });
         return;
       } else {
         Toast.show({
