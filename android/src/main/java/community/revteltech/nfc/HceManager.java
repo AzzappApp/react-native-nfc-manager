c0137c127711554fbcf758b45cacd68d6194d318
fahbe <shecart.mydev@gmail.com>
2025-06-05 13:09:50 +0200

    ifx vcard removal

--- android/src/main/java/community/revteltech/nfc/HceManager.java
+++ android/src/main/java/community/revteltech/nfc/HceManager.java
@@ -2,26 +2,15 @@ package community.revteltech.nfc;
 
 import android.app.Activity;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.nfc.NfcAdapter;
 import android.nfc.cardemulation.CardEmulation;
-import android.nfc.cardemulation.HostApduService;
-import android.os.Build;
 import android.util.Log;
 
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
-
-import java.util.List;
-import org.json.JSONObject;
 
 public class HceManager extends ReactContextBaseJavaModule {
     private static final String TAG = "HceManager";
@@ -106,50 +95,10 @@ public class HceManager extends ReactContextBaseJavaModule {
         try {
             CardEmulation cardEmulation = CardEmulation.getInstance(mNfcAdapter);
             ComponentName componentName = new ComponentName(mReactContext, HceService.class);
-            boolean success = cardEmulation.setDefaultServiceForCategory(componentName, CardEmulation.CATEGORY_OTHER);
-            promise.resolve(success);
+            boolean isEnabled = cardEmulation.isDefaultServiceForCategory(componentName, CardEmulation.CATEGORY_OTHER);
+            promise.resolve(isEnabled);
         } catch (Exception e) {
             promise.reject("ERR_SET_DEFAULT_SERVICE", e.getMessage());
         }
-    }
-
-    @ReactMethod
-    public void setVCardData(String vcardData, Promise promise) {
-        if (mNfcAdapter == null || !mNfcAdapter.isEnabled()) {
-            promise.reject("NFC_NOT_ENABLED", "NFC is not enabled");
-            return;
-        }
-
-        try {
-            Intent intent = new Intent(HceService.ACTION_APDU_RECEIVED);
-            intent.putExtra(HceService.EXTRA_VCARD_DATA, vcardData);
-            mReactContext.sendBroadcast(intent);
-            promise.resolve(true);
-        } catch (Exception e) {
-            promise.reject("VCARD_SET_FAILED", "Failed to set vCard data: " + e.getMessage());
-        }
-    }
-
-    @ReactMethod
-    public void clearVCardData(Promise promise) {
-        if (mNfcAdapter == null || !mNfcAdapter.isEnabled()) {
-            promise.reject("NFC_NOT_ENABLED", "NFC is not enabled");
-            return;
-        }
-
-        try {
-            Intent intent = new Intent(HceService.ACTION_APDU_RECEIVED);
-            intent.putExtra(HceService.EXTRA_VCARD_DATA, (String) null);
-            mReactContext.sendBroadcast(intent);
-            promise.resolve(true);
-        } catch (Exception e) {
-            promise.reject("VCARD_CLEAR_FAILED", "Failed to clear vCard data: " + e.getMessage());
-        }
-    }
-
-    private void sendEvent(String eventName, WritableMap params) {
-        mReactContext
-            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-            .emit(eventName, params);
     }
 } 
